d0874d2b68d253da5d5b8f54277cf0bdb7e1b1d1
M7almazyad <M7almazyad@gmail.com>
2020-06-25 19:14:46 +0300

    Updates

---
+++ precache-manifest.226ff5994f02c1f3d79019e0fcbd413a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b68aa2a01de54e4158ceff3277d9bd",
+    "url": "/Todo2/index.html"
+  },
+  {
+    "revision": "c7d68f9781424c7963ad",
+    "url": "/Todo2/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9e29cc17df8521fe56ea",
+    "url": "/Todo2/static/css/main.2347a438.chunk.css"
+  },
+  {
+    "revision": "c7d68f9781424c7963ad",
+    "url": "/Todo2/static/js/2.ea60507e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Todo2/static/js/2.ea60507e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e29cc17df8521fe56ea",
+    "url": "/Todo2/static/js/main.159aee08.chunk.js"
+  },
+  {
+    "revision": "1859f708f17c60c63d00",
+    "url": "/Todo2/static/js/runtime-main.350d37ef.js"
+  }
+]);
